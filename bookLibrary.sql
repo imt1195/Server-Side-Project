f7ec5dac02135d4bdb657fb7cff22242de3e8404
unknown <Ian>
2021-03-25 11:39:08 -0400

    Updated Readme file with ERB model and uploaded first version of the PostgreSQL database

---
+++ bookLibrary.sql
@@ -0,0 +1,24 @@
+CREATE TABLE "Library" (
+  "id" SERIAL UNIQUE PRIMARY KEY,
+  "LibraryName" varChar,
+  "Location" varchar
+);
+
+CREATE TABLE "Books" (
+  "id" SERIAL UNIQUE PRIMARY KEY,
+  "Title" varchar NOT NULL,
+  "Authot" varchar NOT NULL,
+  "YearPublished" year,
+  "Genre" varchar,
+  "Pages" int
+);
+
+CREATE TABLE "Library_Books" (
+  "id" SERIAL UNIQUE PRIMARY KEY,
+  "Library_Id" int,
+  "Book_Id" int
+);
+
+ALTER TABLE "Library_Books" ADD FOREIGN KEY ("Library_Id") REFERENCES "Library" ("id");
+
+ALTER TABLE "Library_Books" ADD FOREIGN KEY ("Book_Id") REFERENCES "Books" ("id");
