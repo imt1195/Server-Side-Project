04cb3de1e7343647b36477e6b5d07c12f7ea8e5d
Jaylene Bautista <jaylenebautista@Jaylenes-MacBook-Pro-5.local>
2021-03-25 11:49:09 -0400

    db functions

--- db.js
+++ db.js
@@ -12,6 +12,68 @@ const db = pg({
 })
 
 //CRUD 
+//Get all books
+const getBooks = (req,res) => {
+  db.query('SELECT * FROM books ORDER BY id ASC', (error,results) => {
+    if(error){
+      throw error
+    }
+    res.status(200).json(results.row)
+  })
+}
 
+//get book by id 
+const getBookById = (req,res) => {
+  const id = parseInt(req.params.id)
 
-module.exports = db;
+  db.query('SELECT * FROM books WHERE id=$1', (error,results) => {
+    if(error){
+      throw error
+    }
+    res.status(200).json(results.row)
+  })
+}
+
+//create/add a new book
+const createBook = (req,res) => {
+  //name of book/title
+  const { title } = req.body
+
+  db.query('INSERT INTO books (title) VALUES ($1)', [title], (error, results) => {
+    if(error){
+      throw error
+    }
+    res.status(201).send(`Book was added with ID: ${results.insertId}`)
+  })
+}
+
+//update book data
+const updateBook = (req,res) => {
+  const id = parseInt(req.params.id)
+  const { title } = req.body
+  db.query('UPDATE book SET title =$1 WHERE id=$2',[title], (error,results) => {
+    if(error){
+      throw error
+    }
+    res.status(200).send(`book modified with ID: ${id}`)
+  })
+}
+
+//delete a book
+const deleteBook = (req,res) => {
+  const id = parseInt(req.params.id)
+  db.query('DELETE FROM book WHERE id=$1', [id], (error, results) => {
+    if(error){
+      throw error
+    }
+    res.status(200).send(`book deleted with ID: ${id}`)
+  })
+}
+
+module.exports = {
+  getBooks,
+  getBookById,
+  createBook,
+  updateBook,
+  deleteBook,
+}
--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const app = express();
-app.use(express.json());
-
+const bodyParser = require("body-parser")
 const PORT = process.env.PORT || 3000;
 
+// app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+//set up HTTP methods, endpointURL and relevant functions
+app.get('/books', db.getBooks) //get all books
+app.get('/books/:id', db.getBookById) //get book by id
+app.post('/books', db.createbook) //add book 
+app.put('/books/:id', db.updateBook) //update a book was it read?
+app.delete('/books/:id', db.deleteBook) //delete a book
 
 app.listen(PORT, () => {
     console.log(`listenining on http://localhost:${PORT}`);
   });
-
-  
