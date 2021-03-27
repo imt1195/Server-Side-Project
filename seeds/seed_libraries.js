3a51646f8d33b1a1f78a9374150a456d3c498e4a
Ian Tobin <imt1195@gmail.com>
2021-03-27 11:34:56 -0400

    Add files via upload

---
+++ seeds/seed_libraries.js
@@ -0,0 +1,12 @@
+exports.seed = function(knex, Promise){
+    return knex('libraries').del()
+        .then(function(){
+            return knex('libraries').insert([
+                {id:1, LibraryName: "Brooklyn Library", Location: "Brooklyn", LeadLibrarian = "John Smith"},
+                {id:2, LibraryName: "Manhattan Library", Location: "Manhattan", LeadLibrarian = "Henry J. Waternoose III"},
+                {id:3, LibraryName: "Queens Library", Location: "Queens", LeadLibrarian = "Jane Doe"},
+                {id:4, LibraryName: "Bronx Library", Location: "Brnx", LeadLibrarian = "Tibbles the Kitten"},
+                {id:1, LibraryName: "Staten Island Library", Location: "Staten Island", LeadLibrarian = "Bilbo Baggins"}
+            ])
+        })
+}
