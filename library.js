853ba24e07e4ca3296daee1332725b12405b8ed5
Ian Tobin <imt1195@gmail.com>
2021-03-27 16:09:00 -0400

    Update library.js

--- library.js
+++ library.js
@@ -9,19 +9,20 @@ class Library{
     this.LeadLibrarian = LeadLibrarian;
   }
 //find the books at each specific Library
-  static async getLibraryCatalog(id){
-    // try{
-    const bookList = await db.query('select title, author, library.id, LibraryName, Location, from library inner where library.id=$1 join books on Library_Id = library.id;')
-    return res.json(bookList)
-    // }catch(error){
-    //   res.status(500).send(err)
-    // }
+  static async getLibraryCatalog(LibrId, books){
+    try{
+    //let bookList = await db.query('select title, author, library.id =$1, libraryname, Location, from library, books where books.library_id = library.id;', id)
+    let bookList = await db.query('select * from books join library on library.id = books.library_id and books.library_id = LibrId')
+    return bookList
+    }catch(error){
+      console.log(error)
+    }
   }
 
   //list out the libraries: Names, Locations, and Lead Librarians
   static async getLibraries(){
       const LibraryList = await db.query('select * from library order by id asc;')
-      return res.json(LibraryList)
+      return LibraryList
   }
 
 }
